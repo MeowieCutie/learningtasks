3754b63823abd92033e450d2098a50c984c6590d
MeowieCutie <100129730+MeowieCutie@users.noreply.github.com>
2022-04-26 12:29:22 +0800

    Add files via upload

---
+++ LT5/Javascript/Script.js
@@ -0,0 +1,9 @@
+var name = prompt("What is your name?")
+alert ("Welcome " + name + "! Enjoy our Tala by Kyla Collections" );
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
